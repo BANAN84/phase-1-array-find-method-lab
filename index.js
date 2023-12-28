480cbd4a245182edac18e9466ae27b4312b84bfb
banan <banan.faouri2001@gmail.com>
2023-12-28 19:52:41 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+  
+  function check(n){
+   return n.result==="W"
+  }
+function superbowlWin(record){
+    let newarr=record.find(check)
+        
+    return newarr? newarr.year : undefined ;
+}
